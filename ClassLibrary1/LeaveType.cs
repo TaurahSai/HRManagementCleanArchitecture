df66aaab64ac1b2d57333b20768cb2a757d8e053
Keshav <keshavtaurah@yahoo.com>
2024-09-09 19:57:39 +0400

    add domain models

---
+++ ClassLibrary1/LeaveType.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Common;
+
+namespace Domain;
+
+public class LeaveType : BaseEntity
+{
+    public string Name { get; set; } = string.Empty;
+    public int DefaultDays { get; set; }
+}
---
+++ HR.LeaveManagement.Domain/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace HR.LeaveManagement.Domain;
+
+public class Class1
+{
+
+}
