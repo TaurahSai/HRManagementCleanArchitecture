d7e40d54a6a8e21d08f4448d9da641a4d8eb6a6b
Keshav <keshavtaurah@yahoo.com>
2024-09-12 20:21:29 +0400

    Add entityframework and database context

---
+++ Persistence/DatabaseContext/HRDatabaseContext.cs
@@ -0,0 +1,37 @@
+﻿using Domain;
+using Domain.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.DatabaseContext;
+
+public class HRDatabaseContext : DbContext
+{
+    public DbSet<LeaveType> LeaveTypes { get; set; }
+    public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+    public DbSet<LeaveRequest> LeaveRequests { get; set; }
+    public HRDatabaseContext(DbContextOptions<HRDatabaseContext> options) : base(options)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(HRDatabaseContext).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        foreach (var entry in base.ChangeTracker.Entries<BaseEntity>()
+            .Where(q => q.State == EntityState.Added || q.State == EntityState.Modified))
+        {
+            entry.Entity.DateModified = DateTime.UtcNow;
+
+            if (entry.State == EntityState.Added)
+            {
+                entry.Entity.DateCreated = DateTime.UtcNow;
+            }
+        }
+        return base.SaveChangesAsync(cancellationToken);
+    }
+}
--- Persistence/PersistenceServiceRegistration.cs
+++ Persistence/PersistenceServiceRegistration.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.DatabaseContext;
 
 namespace Persistence;
 
 public static class PersistenceServiceRegistration
 {
-    public static IServiceCollection AddPersistenceServices(this IServiceCollection services)
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddDbContext<HRDatabaseContext>(Options =>
+        {
+            Options.UseSqlServer(configuration.GetConnectionString("HrDatabaseConnectionString"));
+        });
+
         return services;
     }
 }
