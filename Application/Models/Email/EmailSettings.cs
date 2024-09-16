8160618f457b1538fe2933c5ab00060c766c76d4
Keshav <keshavtaurah@yahoo.com>
2024-09-16 21:12:38 +0400

    refactor option pattern

--- Application/Models/Email/EmailSettings.cs
+++ Application/Models/Email/EmailSettings.cs
@@ -2,7 +2,6 @@
 
 public class EmailSettings
 {
-    public const string EmailSetting = "EmailSettings";
     public string ApiKey {  get; set; }
     public string FromAddress { get; set; }
 
--- Infrastructure/InfrastructureServicesRegistration.cs
+++ Infrastructure/InfrastructureServicesRegistration.cs
@@ -12,7 +12,7 @@ public static class InfrastructureServicesRegistration
 {
     public static IServiceCollection ConfigureInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
-        services.Configure<EmailSettings>(configuration.GetSection(EmailSettings.EmailSetting));
+        services.Configure<EmailSettings>(configuration.GetSection(nameof(EmailSettings)));
         services.AddTransient<IEmailSender, EmailSender>();
         services.AddScoped(typeof(IAppLogger<>), typeof(LoggerAdapter<>));
         return services;
