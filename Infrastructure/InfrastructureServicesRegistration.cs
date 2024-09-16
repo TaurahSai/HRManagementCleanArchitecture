0b4fe41fb89cb1ac9da741be9dbb35b40bbaffe7
Keshav <keshavtaurah@yahoo.com>
2024-09-16 20:01:21 +0400

    add third party email services

--- Infrastructure/InfrastructureServicesRegistration.cs
+++ Infrastructure/InfrastructureServicesRegistration.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Application.Contracts.Email;
+using Application.Models.Email;
+using Infrastructure.EmailService;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Infrastructure
@@ -7,6 +10,8 @@ namespace Infrastructure
     {
         public static IServiceCollection ConfigureInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
         {
+            services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+            services.AddTransient<IEmailSender, EmailSender>();
             return services;
 
         }
